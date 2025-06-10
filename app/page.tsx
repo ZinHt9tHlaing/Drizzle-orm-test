e4ce94f0d1ed0b12efbdac0049f327f1a0c52418
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-06-10 11:10:00 +0630

    create data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { readTodo } from "@/server/actions";
+import { createTodo, readTodo } from "@/server/actions";
 
 export default async function Home() {
   const { error, success } = await readTodo();
@@ -9,10 +9,20 @@ export default async function Home() {
     <main>
       <h1 className="text-xl font-bold">Todos</h1>
       {success?.map((todo) => (
-        <div key={todo.id}>
-          <p className="text-sm">{todo.title}</p>
-        </div>
+        <p key={todo.id} className="text-sm">
+          {todo.title}
+        </p>
       ))}
+      <div>
+        <form action={createTodo}>
+          <input
+            type="text"
+            name="todoTitle"
+            className="bg-transparent border-2 border-white"
+          />
+          <button type="submit">Add new todo</button>
+        </form>
+      </div>
     </main>
   );
 }
--- server/actions.ts
+++ server/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { db } from ".";
+import { todos } from "./schema";
 
 export const readTodo = async () => {
   const todos = await db.query.todos.findMany();
@@ -10,4 +12,14 @@ export const readTodo = async () => {
   }
 
   return { success: todos };
+};
+
+export const createTodo = async (formData: FormData) => {
+  const todoTitle = formData.get("todoTitle")?.toString();
+  if (!todoTitle) return { error: "No Todo title found" };
+
+  await db.insert(todos).values({ title: todoTitle });
+  revalidatePath("/");
+
+  return { success: "Todo created successfully" };
 };
